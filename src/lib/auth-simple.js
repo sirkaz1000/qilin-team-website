10d63c3f290707be8785a03a149d98e1f0e5d12a
Sir Kaz <sirkaz1000@users.noreply.github.com>
2026-05-29 06:12:40 +0300

    Simplify password requirements to only require 6 characters

--- src/lib/auth-simple.js
+++ src/lib/auth-simple.js
@@ -122,24 +122,8 @@ module.exports = {
   validatePasswordStrength: function(password) {
     const errors = []
     
-    if (password.length < 8) {
-      errors.push('Password must be at least 8 characters long')
-    }
-    
-    if (!/[A-Z]/.test(password)) {
-      errors.push('Password must contain at least one uppercase letter')
-    }
-    
-    if (!/[a-z]/.test(password)) {
-      errors.push('Password must contain at least one lowercase letter')
-    }
-    
-    if (!/[0-9]/.test(password)) {
-      errors.push('Password must contain at least one number')
-    }
-    
-    if (!/[^A-Za-z0-9]/.test(password)) {
-      errors.push('Password must contain at least one special character')
+    if (password.length < 6) {
+      errors.push('Password must be at least 6 characters long')
     }
     
     return {
